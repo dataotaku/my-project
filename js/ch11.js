4365ea691513ef48a9cd282a9ad78823add0baa7
unho.chang <capcock@snu.ac.kr>
2024-06-24 14:05:53 +0900

    ch11 실습 확인 및 종료 커밋

--- js/ch11.js
+++ js/ch11.js
@@ -1,5 +1,5 @@
 const items = document.querySelectorAll("article")
-const aside = document.querySelector("aide");
+const aside = document.querySelector("aside");
 const close = aside.querySelector("span")
 
 for (let el of items) {
@@ -18,18 +18,17 @@ for (let el of items) {
         // aside 요소 안쪽의 콘텐츠에 위의 변수를 적용
         aside.querySelector("h1").innerText = tit;
         aside.querySelector("p").innerText = txt;
-        aside.querySelector("video").setAttribute("src", vidSrc)
+        aside.querySelector("video").setAttribute("src", vidSrc);
 
         // aside 요소 안쪽의 동영상을 재생하고, aside요소에 on을 붙여 패널 활성화
-        aside.querySelector("video").play()
-        aside.classList.add("on")
-
-        //닫기 버튼 클릭 시 
-        close.addEventListener("click", () => {
-            //aside 요소에 클래스 on을 제거해 비활성화 하고 안쪽의 영상을 재생중지
-            aside.classList.remove("on");
-            aside.querySelector("video").pause();
-        });
+        aside.querySelector("video").play();
+        aside.classList.add("on");
+    });
+    //닫기 버튼 클릭 시 
+    close.addEventListener("click", () => {
+        //aside 요소에 클래스 on을 제거해 비활성화 하고 안쪽의 영상을 재생중지
+        aside.classList.remove("on");
+        aside.querySelector("video").pause();
     });
 }
 
